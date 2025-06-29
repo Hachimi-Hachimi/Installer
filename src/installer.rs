cd3da6a6800e3938740566dce9d1f90c893f9d9c
LeadRDRK <leadrdrk@zohomail.com>
2025-06-29 20:59:11 +0700

    Global support

--- src/installer.rs
+++ src/installer.rs
@@ -1,8 +1,6 @@
-use std::{fs::File, io::Write, path::{Path, PathBuf}};
+use std::{fs::File, io::Write, path::PathBuf};
 
 use pelite::resources::version_info::Language;
-use tinyjson::JsonValue;
-use windows::Win32::UI::Shell::{FOLDERID_RoamingAppData, SHGetKnownFolderPath, KF_FLAG_DEFAULT};
 
 use crate::utils;
 
@@ -16,54 +14,15 @@ impl Installer {
     pub fn custom(install_dir: Option<PathBuf>, target: Option<String>) -> Installer {
         Installer {
             install_dir: install_dir.or_else(Self::detect_install_dir),
-            target: Target::Dxgi,
+            target: Target::Winhttp,
             custom_target: target
         }
     }
 
     fn detect_install_dir() -> Option<PathBuf> {
-        let app_data_dir_wstr = unsafe { SHGetKnownFolderPath(&FOLDERID_RoamingAppData, KF_FLAG_DEFAULT, None).ok()? };
-        let app_data_dir_str = unsafe { app_data_dir_wstr.to_string().ok()? };
-        let app_data_dir = Path::new(&app_data_dir_str);
-        let mut dmm_config_path = app_data_dir.join("dmmgameplayer5");
-        dmm_config_path.push("dmmgame.cnf");
-
-        let config_str = std::fs::read_to_string(dmm_config_path).ok()?;
-        let JsonValue::Object(config) = config_str.parse().ok()? else {
-            return None;
-        };
-        let JsonValue::Array(config_contents) = &config["contents"] else {
-            return None;
-        };
-        for value in config_contents {
-            let JsonValue::Object(game) = value else {
-                return None;
-            };
-
-            let JsonValue::String(product_id) = &game["productId"] else {
-                continue;
-            };
-            if product_id != "umamusume" {
-                continue;
-            }
-
-            let JsonValue::Object(detail) = &game["detail"] else {
-                return None;
-            };
-            let JsonValue::String(path_str) = &detail["path"] else {
-                return None;
-            };
-
-            let path = PathBuf::from(path_str);
-            return if path.is_dir() {
-                Some(path)
-            }
-            else {
-                None
-            }
-        }
-
-        None
+        let steam_dir = steamlocate::SteamDir::locate().ok()?;
+        let (umamusume, library) = steam_dir.find_app(3224770).ok().flatten()?;
+        Some(library.resolve_app_dir(&umamusume))
     }
 
     pub fn get_target_path(&self, target: Target) -> Option<PathBuf> {
@@ -122,9 +81,9 @@ impl Installer {
 
     pub fn get_hachimi_installed_target(&self) -> Option<Target> {
         for target in Target::VALUES {
-            if let Some(version_info) = self.get_target_version_info(target) {
+            if let Some(version_info) = self.get_target_version_info(*target) {
                 if version_info.is_hachimi() {
-                    return Some(target);
+                    return Some(*target);
                 }
             }
         }
@@ -155,7 +114,7 @@ impl Default for Installer {
     fn default() -> Installer {
         Installer {
             install_dir: Self::detect_install_dir(),
-            target: Target::Dxgi,
+            target: Target::Winhttp,
             custom_target: None
         }
     }
@@ -163,16 +122,18 @@ impl Default for Installer {
 
 #[derive(Clone, Copy, Eq, PartialEq)]
 pub enum Target {
-    Dxgi,
+    Winhttp,
+    Version,
     Opengl32
 }
 
 impl Target {
-    pub const VALUES: [Self; 2] = [Self::Dxgi, Self::Opengl32];
+    pub const VALUES: &[Self] = &[Self::Winhttp, Self::Version, Self::Opengl32];
 
     pub fn dll_name(&self) -> &'static str {
         match self {
-            Self::Dxgi => "dxgi.dll",
+            Self::Winhttp => "winhttp.dll",
+            Self::Version => "version.dll",
             Self::Opengl32 => "opengl32.dll"
         }
     }
